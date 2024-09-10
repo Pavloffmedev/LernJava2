2b81bacc4ff94615aaa891e5ab584c2c9ba4c1eb
pavelkiruhin <pavelkiruhin@icloud.com>
2024-09-10 12:04:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,128 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * Задание 1:
+     */
+    static void Task1() {}
+
+
+    /**
+     * Задание 2:
+     */
+    static void Task2() {}
+
+
+    /**
+     * Задание 3:
+     */
+    static void Task3() {}
+
+
+    /**
+     * Задание 4:
+     */
+    static void Task4() {}
+
+
+    /**
+     * Задание 5:
+     */
+    static void Task5() {}
+
+
+    /**
+     * Задание 6:
+     */
+    static void Task6() {}
+
+
+    /**
+     * Задание 7:
+     */
+    static void Task7() {}
+
+
+    /**
+     * Задание 8:
+     */
+    static void Task8() {}
+
+
+    /**
+     * Задание 9:
+     */
+    static void Task9() {}
+
+
+    /**
+     * Задание 10:
+     */
+    static void Task10() {}
+
+
+    /**
+     * Задание 11:
+     */
+    static void Task11() {}
+
+
+    /**
+     * Задание 12:
+     */
+    static void Task12() {}
+
+
+    /**
+     * Задание 13:
+     */
+    static void Task13() {}
+
+
+    /**
+     * Задание 14:
+     */
+    static void Task14() {}
+
+
+    /**
+     * Задание 15:
+     */
+    static void Task15() {}
+
+
+    /**
+     * Задание 16:
+     */
+    static void Task16() {}
+
+
+    /**
+     * Задание 17:
+     */
+    static void Task17() {}
+
+
+    /**
+     * Задание 18:
+     */
+    static void Task18() {}
+
+
+    /**
+     * Задание 19:
+     */
+    static void Task19() {}
+
+
+    /**
+     * Задание 20:
+     */
+    static void Task20() {}
+
+}
